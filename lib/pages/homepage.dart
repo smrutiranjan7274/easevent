aaae78c1a0dea0c0161e6aa68f0eeaeb1a993c1c
Smruti Ranjan Bhanja <smrutibhanja32@gmail.com>
2022-11-09 15:21:30 +0530

    Base Project

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: FloatingActionButton(
+          onPressed: () {
+            _incrementCounter();
+          },
+          mini: true,
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
